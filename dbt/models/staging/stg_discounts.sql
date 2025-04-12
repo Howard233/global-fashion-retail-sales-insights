7cb0ec9824d223a8185200e034dadee9e68df95a
Haozhe Wang <haozhewang1996@gmail.com>
2025-04-12 18:58:26 +0000

    dbt development

---
+++ dbt/models/staging/stg_discounts.sql
@@ -0,0 +1,9 @@
+{{ config(materialized="view") }}
+
+with discounts as (select * from {{ source("staging", "discounts") }})
+-- exclude some columns that are not very helpful
+select start_date, end_date, discount, category, sub_category
+from discounts
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var("is_test_run", default=true) %} limit 100 {% endif %}
