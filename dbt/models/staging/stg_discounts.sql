7fa6e975d198288181fc0a6e88133ced38faef3c
Haozhe Wang <haozhewang1996@gmail.com>
2025-04-12 19:33:56 +0000

    comment default test

--- dbt/models/staging/stg_discounts.sql
+++ dbt/models/staging/stg_discounts.sql
@@ -6,4 +6,4 @@ select start_date, end_date, discount, category, sub_category
 from discounts
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var("is_test_run", default=true) %} limit 100 {% endif %}
+-- {% if var("is_test_run", default=true) %} limit 100 {% endif %}
--- dbt/models/staging/stg_employees.sql
+++ dbt/models/staging/stg_employees.sql
@@ -6,4 +6,4 @@ select employee_id, store_id, position
 from employees
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var("is_test_run", default=true) %} limit 100 {% endif %}
+-- {% if var("is_test_run", default=true) %} limit 100 {% endif %}
